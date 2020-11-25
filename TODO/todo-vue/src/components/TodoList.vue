9a8a4eefcba38af086ef18e7473d40e0ee394d72
sergemad <sergekeita01@gmail.com>
2020-11-25 22:59:46 +0100

    TODOList finish

--- TODO/todo-vue/src/components/TodoList.vue
+++ TODO/todo-vue/src/components/TodoList.vue
@@ -2,7 +2,8 @@
     <div>
         <input type="text" class="todo-input" placeholder="what needs to be done"
         v-model="newtodo" @keyup.enter="Addtodo()" />
-        <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item" >
+        <transition-group name="fade" enter-active-class="animated fadeInUp" leave-active-class="animated fadeOutDown">
+        <div v-for="(todo, index) in todoFiltered" :key="todo.id" class="todo-item" >
             <div class="remove-item" @click="removetodo(index)">
                 &times;
             </div>
@@ -17,6 +18,27 @@
             </div>
             
         </div>
+        </transition-group>
+        <div class="extra-container">
+            <div>
+                <input type="checkbox" :checked="AnyNotDone" @click="CheckAll()">
+                Check all
+            </div>
+            
+            <div>
+                {{NotDone}} item left
+            </div>
+        </div>
+        <div class="extra-container">
+            <div>
+                <input type="button" class="button" @click="Filtered = 'All'" value="All">
+                <input type="button" class="button" @click="Filtered = 'Active'" value="Active">
+                <input type="button" class="button" @click="Filtered = 'Completed'" value="Completed">
+            </div>
+            <transition name="fade">
+            <input type="button" class="button" v-if="OneDone" @click="ClearCompleted()" value="Clear completed">
+            </transition>
+        </div>
     </div>
 </template>
 
@@ -28,6 +50,7 @@ export default {
       newtodo:'',
       idForNewTodo: 3,
       BeforeTitleEdit: '',
+      Filtered:'All',
       todos:[
           {
               id:1,
@@ -84,8 +107,37 @@ export default {
       canceledite(todo){
           todo.title=this.BeforeTitleEdit
           todo.edite=false
+      },
+      CheckAll(){
+          this.todos.forEach((todo) => todo.completed=event.target.checked)
+      },
+      ClearCompleted(){
+          this.todos=this.todos.filter(todo => !todo.completed)
       }
+
     
+  },
+
+  computed: {
+      NotDone(){
+          return this.todos.filter(todo => !todo.completed).length
+      },
+      AnyNotDone(){
+          return this.NotDone ==0
+      },
+      OneDone(){
+          return this.todos.filter(todo => todo.completed).length > 0
+      },
+      todoFiltered(){
+          if(this.Filtered=='All'){
+              return this.todos
+          }else if(this.Filtered=='Active'){
+              return this.todos.filter(todo => !todo.completed)
+          }else if(this.Filtered=='Completed'){
+              return this.todos.filter(todo => todo.completed)
+          }
+          return this.todos
+      }
   },
   
 
@@ -94,6 +146,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style >
+@import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css");
+
 .todo-input{
     border: 1px solid black;
     padding: 10px;
@@ -106,8 +160,8 @@ export default {
     float: left;
     justify-content: space-between;
     align-items: center;
-    margin: 14px;
     max-width: 500px; 
+    animation-duration: 0.3s;
 }
 .remove-item{
     cursor: pointer;
@@ -127,10 +181,33 @@ export default {
     display: flex;
     align-items: center;
     padding: 10px;
+    
 }
 .completed{
     text-decoration: line-through;
     color: grey;
 }
+.extra-container{
+    display: flex;
+    justify-content: space-between;
+    font-size: 16px;
+    border-top: 1px solid lightgrey;
+    padding: 10px;
+}
+.button{
+    border: none;
+    background: white;
+}
+.button:hover{
+    cursor: pointer;
+    background-color: teal;
+    color: white;
+}
+.fade-enter-active, .fade-leave-active{
+    transition:opacity .2s;
+} 
+.fade-enter, .fade-leave{
+    opacity: 0;
+} 
 
 </style>
